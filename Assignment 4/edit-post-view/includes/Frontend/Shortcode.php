5a03ef28b4c892dc6a48c053cfece67534693611
Khalidwebmail <csewebmail@gmail.com>
2021-03-11 16:30:18 +0600

    Code fix assignment_4_edit-post-view plg

--- Assignment 4/edit-post-view/includes/Frontend/Shortcode.php
+++ Assignment 4/edit-post-view/includes/Frontend/Shortcode.php
@@ -20,24 +20,37 @@ class Shortcode {
     public function render_count_shortcode( $atts )
     {
         $atts = shortcode_atts( 
-            array (
-            'posts_per_page' => 10,
-            'category_name'  => null,
-            'post_id'        => '',
-            'post_type'      => 'post',
-            'order'          => 'DESC',
-            'post_status'    => 'publish',
-        ), $atts );
+            [
+                'posts_per_page' => 10,
+                'category_name'  => '',
+                'post_id'        => '',
+                'post_type'      => 'post',
+                'order'          => 'DESC',
+                'post_status'    => 'publish',
+            ], $atts );
 
         $ids = $atts[ 'post_id' ];
+        $cat = $atts[ 'category_name' ];
 
-        if( $ids != '' ) {
+        if( '' !== $cat ) {
+            $atts[ 'post__in' ] = explode( ',', $cat );
+            $the_query = new \WP_Query( $atts );
+            if( $the_query->have_posts() ) {
+                while ( $the_query->have_posts() ) {
+                    $the_query->the_post();
+                    echo '<a style="color: darkorange" href=" '. get_permalink() .' "><h2 class="entry-title">'. get_the_title().' ( '.$the_query->post->post_views_count.' )</h2></a>';
+                }
+                wp_reset_postdata();
+            }
+        }
+
+        if( '' !== $ids ) {
             $atts[ 'post__in' ] = explode( ',', $ids );
             $the_query = new \WP_Query( $atts );
             if( $the_query->have_posts() ) {
                 while ( $the_query->have_posts() ) {
                     $the_query->the_post();
-                    echo '<a style="color: darkorange" href="'.get_permalink().'"><h2 class="entry-title">'. get_the_title().' ('.$the_query->post->post_views_count.')</h2></a>';
+                    echo '<a style="color: darkorange" href=" '. get_permalink() .' "><h2 class="entry-title">'. get_the_title().' ( '.$the_query->post->post_views_count.' )</h2></a>';
                 }
                 wp_reset_postdata();
             }
@@ -50,7 +63,7 @@ class Shortcode {
             if( $the_query->have_posts() ) {
                 while ( $the_query->have_posts() ) {
                     $the_query->the_post();
-                    echo '<a style="color: darkorange" href="'.get_permalink().'"><h2 class="entry-title">'. get_the_title().' ('.$the_query->post->post_views_count.')</h2></a>';
+                    echo '<a style="color: darkorange" href=" '. get_permalink() .' "><h2 class="entry-title">'. get_the_title().' ( '.$the_query->post->post_views_count.' )</h2></a>';
                 }
                 wp_reset_postdata();
             }
