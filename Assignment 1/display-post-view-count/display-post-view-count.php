0de6b3aaef7eefa17679459c762c53b757f7d873
Khalidwebmail <csewebmail@gmail.com>
2021-03-25 09:49:11 +0600

    add doc comment display post view count

--- Assignment 1/display-post-view-count/display-post-view-count.php
+++ Assignment 1/display-post-view-count/display-post-view-count.php
@@ -8,92 +8,91 @@
    Author URI:      https://example.com/
    License:         GPL2 or later
    Text domain:     display-simple-post-view-count
-   */
+*/
 
-use Display\Post\View\Frontend\Counter;
 
 if( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+use Display\Viewers\Frontend\Counter;
 require_once __DIR__ . '/vendor/autoload.php';
 
+/**
+ * Display_Post_View_Count
+ * Main skaleton of this plugin
+ */
 final class Display_Post_View_Count {
-
     const version = '1.0.0';
-
+    
+    /**
+     * __construct
+     * Define construct
+     * @return void
+     */
     private function __construct() {
-
-        $this->define_constant();
-
-        register_activation_hook( __FILE__, [$this, 'activate'] );
-
-        add_action( 'init', [$this, 'init_plugin'] );
+        $this->wd_dpvc_define_constant();
+        register_activation_hook( __FILE__, [ $this, 'wd_dpvc_activate' ] );
     }
-
-        
+    
     /**
      * init
      * initialize single instance
      * @return \Display_Post_View_Count
      */
-    public static function init() {
+    public static function wd_dpvc_init() {
         static $instance = false;
-
         if( ! $instance ) {
-            $instance =new self();
+            $instance = new self();
         }
-
         return $instance;
     }
-    
+
     /**
      * define_constant
-     * DPVC - Display Post View Count
+     * 
      * @return void
      */
-    private function define_constant() {
-        define( 'DPVC_RELEASE_NUMBER', self::version );
-        define( 'DPVC_FILE', __FILE__ );
-        define( 'DPVC_PATH', __DIR__ );
-        define( 'DPVC_URL', plugins_url( '', DPVC_FILE ) );
-        define( 'DPVC_ASSETS', DPVC_URL . '/asstes');
+    private function wd_dpvc_define_constant() {
+        define( 'WD_DPVC_RELEASE_NUMBER', self::version );
+        define( 'WD_DPVC_FILE', __FILE__ );
+        define( 'WD_DPVC_PATH', __DIR__ );
+        define( 'WD_DPVC_URL', plugins_url( '', WD_DPVC_FILE ) );
     }
-        
+
     /**
      * init_plugin
      * initialize plugin
      * @return void
      */
-    public function init_plugin() {
+    public static function wd_dpvc_init_plugin() {
         new Counter();
     }
 
     /**
      * activate
-     *  
+     * Active plugin after install
      * @return void
      */
-    public function activate() {
-        $install_date = get_option( 'dpvc_installed', time() );
+    public function wd_dpvc_activate() {
+        $install_date = get_option( 'wd_dpvc_installed', time() );
         
         if( ! $install_date ) {
-            update_option( 'dpvc_version', DPVC_RELEASE_NUMBER );
+            update_option( 'wd_dpvc_version', WD_DPVC_RELEASE_NUMBER );
         }
     }
-} 
-
+}
 
 /**
- * show_views_number
+ * wd_dpvc_show_post_viewer
  * initialize the main plugin
  * @return \Display_Post_View_Count
  */
-function show_views_number() {
-    return Display_Post_View_Count::init();
+function wd_dpvc_show_post_viewer() {
+    return Display_Post_View_Count::wd_dpvc_init_plugin();
 }
 
 /**
  * kick of the pluging by calling the function
  */
-show_views_number();
+wd_dpvc_show_post_viewer();
--- Assignment 1/display-post-view-count/includes/Frontend/Counter.php
+++ Assignment 1/display-post-view-count/includes/Frontend/Counter.php
@@ -1,34 +1,43 @@
 <?php
 
-namespace Display\Post\View\Frontend;
+namespace Display\Viewers\Frontend;
 
 class Counter {
-    
+
     public function __construct() {
-        add_filter( 'the_content', [ $this, 'dpvc_get_post_view' ] );
-        add_filter( 'the_post', [ $this, 'dpvc_set_post_view' ] );
+        add_filter( 'the_content', [ $this, 'wd_dpvc_show_post_view' ] );
+        add_filter( 'the_post', [ $this, 'wd_dpvc_set_post_view' ] );
     }
-
-	public function dpvc_get_post_view($content) {
-	    $count = get_post_meta( get_the_ID(), 'post_views_count', true );
-	    if( $count > 0) {
-			$tag = apply_filters( 'pb_tag', 'em' );
-			return $content .= sprintf( __( '<h2>Total View : <%s>%s</%s></h2>' ), $tag, $count, $tag );
-		}
-		else{
-			return $content .= sprintf( __( '<h2>Total View : 0' ) );
-		}
-	}
-
-	public function dpvc_set_post_view() {
-
-	    $key = 'post_views_count';
-	    if( is_single() ) {
-	    	$post_id = get_the_ID();
-	    	$count = (int) get_post_meta( $post_id, $key, true );
-			$count++;
-		    update_post_meta( $post_id, $key, $count );
-	    }
-	}
+    
+    /**
+     * wd_dpvc_show_post_view
+     * Get current viewers and return
+     * @param  mixed $content
+     * @return $content
+     */
+    public function wd_dpvc_show_post_view( $content ) {
+        $count = get_post_meta( get_the_ID(), 'post_views_count', true );
+        if( $count ) {
+            $tag = apply_filters( 'wd_dpvc_tag', 'em' );
+            return $content .= wp_sprintf( __( '<h3>Total viewers: <%s>%s<%s></h3>' ), $tag, $count, $tag );
+        }
+        else {
+            return $content .= wp_sprintf( __( '<h2>Total view : 0' ) );
+        }
+    }
+    
+    /**
+     * wd_dpvc_set_post_view
+     * Increment number of single post visit
+     * @return void
+     */
+    public function wd_dpvc_set_post_view() {
+        $key = 'post_views_count';
+        if( is_single() ) {
+            $post_id = get_the_ID();
+            $count = (int) get_post_meta( $post_id, $key, true );
+            $count++;
+            update_post_meta( $post_id, $key, $count );
+        }
+    }
 }
-
